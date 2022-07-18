2c282750f8061ccdc35df9bd027b90769a7e5799
siva50 <80094666+siva50@users.noreply.github.com>
2022-07-18 20:05:48 +0530

    Add files via upload

---
+++ studentgrade.py
@@ -0,0 +1,14 @@
+a=int(input("enter students marks:"))
+if a>90:
+    print("the number is outstanding:")
+
+elif a>80:
+    print("the number is execellent:")
+elif a>60:
+    print("the number is good:")
+elif a>40:
+    print("the number is poor:")
+elif a<40:
+    print("the number is fail:")
+else:
+    print("the number is final")
